712c9efa8d1f198c23ff2040cb7c9a7401bffb38
CyberSpartan4 <xarazon@cbzrc.pshs.edu.ph>
2026-05-11 11:00:42 +0800

    js

---
+++ script.js
@@ -0,0 +1,28 @@
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskList = document.getElementById("taskList");
+
+    if (taskInput.value === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.textContent = taskInput.value;
+
+    li.onclick = function () {
+        li.style.textDecoration = "line-through";
+    };
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+}
